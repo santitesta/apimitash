1f951b362573f438a799258e22fa6c447cf9e6ad
santitesta <testasanty@gmail.com>
2023-02-02 09:56:47 -0300

    Initial configuration

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+if (!process.env.IS_TS_NODE) {
+  require('module-alias/register')
+}
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  await app.listen(3001);
+}
+bootstrap();
