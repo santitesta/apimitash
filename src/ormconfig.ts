57866b603fd1b790d4b31b36d9d41df33326b86a
santitesta <testasanty@gmail.com>
2023-02-02 09:57:03 -0300

    Created ORM config

---
+++ src/ormconfig.ts
@@ -0,0 +1,22 @@
+import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
+import { ConfigModule } from "@nestjs/config";
+ConfigModule.forRoot();
+
+let host: string; // Assign as default value the host of AWS RDS
+if (process.env.NODE_ENV === 'development') {
+  host = 'localhost'
+}
+
+const config: PostgresConnectionOptions = {
+  type: 'postgres',
+  host: host,
+  port: 5432,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASS,
+  database: 'mitash',
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: false,
+  migrations: [__dirname + '/migrations/**/*{.ts,.js}']
+};
+
+export default config;
