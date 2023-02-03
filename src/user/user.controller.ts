20060d022d648f7d42c276073e329163bd378ed3
santitesta <testasanty@gmail.com>
2023-02-03 14:41:18 -0300

    Feat(getAllUsers function created)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,22 +1,32 @@
-import { Body, Controller, Get, Post, Put, UseGuards, UsePipes, ValidationPipe } from "@nestjs/common";
-import { UserService } from "@app/user/user.service";
-import { CreateUserDto } from "@app/user/dto/createUser.dto";
-import { UserResponseInterface } from "./types/userResponse.interface";
-import { LoginUserDto } from "./dto/loginUser.dto";
-import { User } from "@app/user/decorators/user.decorator";
-import { UserEntity } from "./user.entity";
-import { AuthGuard } from "./guards/auth.guard";
-import { UpdateUserDto } from "./dto/updateUser.dto";
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Put,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { UserService } from '@app/user/user.service';
+import { CreateUserDto } from '@app/user/dto/createUser.dto';
+import { UserResponseInterface } from './types/userResponse.interface';
+import { LoginUserDto } from './dto/loginUser.dto';
+import { User } from '@app/user/decorators/user.decorator';
+import { UserEntity } from './user.entity';
+import { AuthGuard } from './guards/auth.guard';
+import { UpdateUserDto } from './dto/updateUser.dto';
 
 @Controller()
 export class UserController {
-  constructor(private readonly userService: UserService) { }
+  constructor(private readonly userService: UserService) {}
 
   // Create user
   @Post('user')
   @UsePipes(new ValidationPipe())
   async createUser(
-    @Body('user') createUserDto: CreateUserDto): Promise<UserResponseInterface> {
+    @Body('user') createUserDto: CreateUserDto,
+  ): Promise<UserResponseInterface> {
     const user = await this.userService.createUser(createUserDto);
     return this.userService.buildUserResponse(user);
   }
@@ -25,19 +35,23 @@ export class UserController {
   @Post('user/login')
   @UsePipes(new ValidationPipe())
   async login(
-    @Body('user') loginDto: LoginUserDto
+    @Body('user') loginDto: LoginUserDto,
   ): Promise<UserResponseInterface> {
-    const user = await this.userService.login(loginDto)
-    return this.userService.buildUserResponse(user)
+    const user = await this.userService.login(loginDto);
+    return this.userService.buildUserResponse(user);
   }
 
   // Get current user
   @Get('user')
   @UseGuards(AuthGuard)
-  async currentUser(
-    @User() user: UserEntity
-  ): Promise<UserResponseInterface> {
+  async currentUser(@User() user: UserEntity): Promise<UserResponseInterface> {
     return this.userService.buildUserResponse(user);
+  }
+
+  // Get all users
+  @Get('users')
+  async getAllUsers(): Promise<UserEntity[]> {
+    return this.userService.getAllUsers();
   }
 
   //Update user
@@ -45,9 +59,12 @@ export class UserController {
   @UseGuards(AuthGuard)
   async updateCurrentUser(
     @User('id') currentUserId: number,
-    @Body('user') updateUserDto: UpdateUserDto
+    @Body('user') updateUserDto: UpdateUserDto,
   ): Promise<UserResponseInterface> {
-    const user = await this.userService.updateUser(currentUserId, updateUserDto);
+    const user = await this.userService.updateUser(
+      currentUserId,
+      updateUserDto,
+    );
     return this.userService.buildUserResponse(user);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,37 +1,38 @@
-import { ConfigModule } from "@nestjs/config";
-import { Injectable } from "@nestjs/common";
-import { CreateUserDto } from "@app/user/dto/createUser.dto";
-import { UserEntity } from "@app/user/user.entity";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
-import { sign } from "jsonwebtoken";
-import { UserResponseInterface } from "./types/userResponse.interface";
-import { HttpException } from "@nestjs/common/exceptions";
-import { HttpStatus } from "@nestjs/common/enums";
-import { LoginUserDto } from "./dto/loginUser.dto";
-import { compare } from "bcrypt";
-import { UpdateUserDto } from "./dto/updateUser.dto";
+import { ConfigModule } from '@nestjs/config';
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from '@app/user/dto/createUser.dto';
+import { UserEntity } from '@app/user/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { sign } from 'jsonwebtoken';
+import { UserResponseInterface } from './types/userResponse.interface';
+import { HttpException } from '@nestjs/common/exceptions';
+import { HttpStatus } from '@nestjs/common/enums';
+import { LoginUserDto } from './dto/loginUser.dto';
+import { compare } from 'bcrypt';
+import { UpdateUserDto } from './dto/updateUser.dto';
 ConfigModule.forRoot();
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(UserEntity)
-    private readonly userRepository: Repository<UserEntity>
-  ) { }
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
 
   // Create user
   async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
     const userByEmail = await this.userRepository.findOne({
-      where: { email: createUserDto.email }
-    })
+      where: { email: createUserDto.email },
+    });
     const userByUsername = await this.userRepository.findOne({
-      where: { username: createUserDto.username }
-    })
+      where: { username: createUserDto.username },
+    });
     if (userByEmail || userByUsername) {
       throw new HttpException(
         'Email or username are taken',
-        HttpStatus.UNPROCESSABLE_ENTITY)
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
     }
     const newUser = new UserEntity();
     Object.assign(newUser, createUserDto);
@@ -40,26 +41,40 @@ export class UserService {
 
   // Get user by Id
   async findById(id: number): Promise<UserEntity> {
-    return this.userRepository.findOne({ where: { id } })
+    return this.userRepository.findOne({ where: { id } });
+  }
+
+  // Get all users
+  async getAllUsers(): Promise<UserEntity[]> {
+    return this.userRepository.find();
   }
 
   // Login
   async login(loginUserDto: LoginUserDto): Promise<UserEntity> {
     const user = await this.userRepository.findOne({
       where: {
-        email: loginUserDto.email
+        email: loginUserDto.email,
       },
-      select: ['id', 'username', 'email', 'password']
-    })
+      select: ['id', 'username', 'email', 'password'],
+    });
 
     if (!user) {
-      throw new HttpException('Credentials are not valid', HttpStatus.UNPROCESSABLE_ENTITY)
+      throw new HttpException(
+        'Credentials are not valid',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
     }
 
-    const isPasswordCorrect = await compare(loginUserDto.password, user.password)
+    const isPasswordCorrect = await compare(
+      loginUserDto.password,
+      user.password,
+    );
 
     if (!isPasswordCorrect) {
-      throw new HttpException('Credentials are not valid', HttpStatus.UNPROCESSABLE_ENTITY)
+      throw new HttpException(
+        'Credentials are not valid',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
     }
 
     delete user.password;
@@ -67,26 +82,32 @@ export class UserService {
   }
 
   // Update user
-  async updateUser(userId: number, updateUserDto: UpdateUserDto): Promise<UserEntity> {
+  async updateUser(
+    userId: number,
+    updateUserDto: UpdateUserDto,
+  ): Promise<UserEntity> {
     const user = await this.findById(userId);
     Object.assign(user, updateUserDto);
     return await this.userRepository.save(user);
   }
 
   generateJwt(user: UserEntity): string {
-    return sign({
-      id: user.id,
-      username: user.username,
-      email: user.email
-    }, process.env.JWT_SECRET)
+    return sign(
+      {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+      },
+      process.env.JWT_SECRET,
+    );
   }
 
   buildUserResponse(user: UserEntity): UserResponseInterface {
     return {
       user: {
         ...user,
-        token: this.generateJwt(user)
-      }
-    }
+        token: this.generateJwt(user),
+      },
+    };
   }
 }
